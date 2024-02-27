54ef10d8cd3291affbf10a6d27a78c11423facb6
Nate-Soul <nanithsukpong@gmail.com>
2024-02-27 08:00:25 +0100

    restyle modifications for input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,24 @@
 //         }
 //     });
 // });
-const timeInputs = document.querySelectorAll("input[type='time']");
-timeInputs.forEach(timeInput => {
-    timeInput.addEventListener("change", () => {
-        const timeDisplay = timeInput.nextElementSibling.querySelector("span");
-        timeDisplay.textContent = timeInput.value;
-    });
-});
-const dateInputs = document.querySelectorAll("input[type='date']");
-dateInputs.forEach(dateInput => {
-    dateInput.addEventListener("input", () => {
-        const dateDisplay = dateInput.nextElementSibling.querySelector("span");
-        if (dateInput.value !== "") {
-            dateDisplay.textContent = dateInput.value;
-        } else {
-            dateDisplay.textContent = "mm / dd / yy"
-        }
-    });
-});
+// const timeInputs = document.querySelectorAll("input[type='time']");
+// timeInputs.forEach(timeInput => {
+//     timeInput.addEventListener("change", () => {
+//         const timeDisplay = timeInput.nextElementSibling.querySelector("span");
+//         timeDisplay.textContent = timeInput.value;
+//     });
+// });
+// const dateInputs = document.querySelectorAll("input[type='date']");
+// dateInputs.forEach(dateInput => {
+//     dateInput.addEventListener("input", () => {
+//         const dateDisplay = dateInput.nextElementSibling.querySelector("span");
+//         if (dateInput.value !== "") {
+//             dateDisplay.textContent = dateInput.value;
+//         } else {
+//             dateDisplay.textContent = "mm / dd / yy"
+//         }
+//     });
+// });
 
 
 const estimatedDurationCbs = document.getElementsByName("estimated_duration");
